f462d9cae5f6389eb5debc6f4e9e4cfdc043aa37
iishasmall <iishasmall@iishasmall.com>
2018-06-19 14:06:02 -0400

    added text

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
 
         <p className="App-intro">
          <p> whatever </p>
+         <p>
+         hello
+         </p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
